b2fa055fc34c526baf13e3b422541f38f2ae4872
Julian C. Dunn <jdunn@chef.io>
2016-10-19 14:24:47 -0400

    Update to mongodb 3.2.9
    Signed-off-by: Julian C. Dunn <jdunn@chef.io>

--- mongodb/plan.sh
+++ mongodb/plan.sh
@@ -1,13 +1,13 @@
 pkg_name=mongodb
 pkg_origin=chefops
-pkg_version=3.2.6
+pkg_version=3.2.9
 pkg_maintainer="Ben Rockwood <benr@chef.io>"
 pkg_description="High-performance, schema-free, document-oriented database"
 pkg_license=('AGPL-3.0')
 pkg_source=https://fastdl.mongodb.org/linux/${pkg_name}-linux-x86_64-${pkg_version}.tgz
 pkg_filename=${pkg_name}-linux-x86_64-${pkg_version}.tgz
 pkg_dirname=${pkg_name}-linux-x86_64-${pkg_version}
-pkg_shasum=2b977dbb222fbeddc31aecba931f726bdc488b2f8ff4314768d2fda5b27e85e3
+pkg_shasum=07d982db0f7e7fb10a8d110d47debc470933d55f2077d7f547f14a5201725d50
 pkg_deps=(core/glibc core/gcc-libs)
 pkg_build_deps=(core/patchelf)
 
